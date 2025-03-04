d366e4425dfcd56ddb0588cdd56ef488327f3a66
nick <nick.m.dodson@gmail.com>
2025-03-04 13:58:53 -0500

    override hb only for s1ap

--- lib/core/ogs-sockopt.c
+++ lib/core/ogs-sockopt.c
@@ -38,15 +38,15 @@ void ogs_sockopt_init(ogs_sockopt_t *option)
 
     memset(option, 0, sizeof *option);
 
-    option->sctp.spp_hbinterval = 120000;         /* 2 minutes */
-    option->sctp.spp_sackdelay = 200;           /* 200 ms */
-    option->sctp.srto_initial = 3000;           /* 3 seconds */
-    option->sctp.srto_min = 1000;               /* 1 seconds */
-    option->sctp.srto_max = 5000;               /* 5 seconds */
+    option->sctp.spp_hbinterval = 5000; /* 5 seconds */
+    option->sctp.spp_sackdelay = 200;   /* 200 ms */
+    option->sctp.srto_initial = 3000;   /* 3 seconds */
+    option->sctp.srto_min = 1000;       /* 1 seconds */
+    option->sctp.srto_max = 5000;       /* 5 seconds */
     option->sctp.sinit_num_ostreams = OGS_DEFAULT_SCTP_MAX_NUM_OF_OSTREAMS;
     option->sctp.sinit_max_instreams = 65535;
     option->sctp.sinit_max_attempts = 4;
-    option->sctp.sinit_max_init_timeo = 8000;   /* 8 seconds */
+    option->sctp.sinit_max_init_timeo = 8000; /* 8 seconds */
 
     option->sctp_nodelay = true;
     option->tcp_nodelay = true;
@@ -60,7 +60,8 @@ int ogs_nonblocking(ogs_socket_t fd)
 
     u_long io_mode = 1;
     rc = ioctlsocket(fd, FIONBIO, &io_mode);
-    if (rc != OGS_OK) {
+    if (rc != OGS_OK)
+    {
         ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno, "ioctlsocket failed");
         return OGS_ERROR;
     }
@@ -70,13 +71,16 @@ int ogs_nonblocking(ogs_socket_t fd)
     ogs_assert(fd != INVALID_SOCKET);
 
     flags = fcntl(fd, F_GETFL, NULL);
-    if (flags < 0) {
+    if (flags < 0)
+    {
         ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno, "F_GETFL failed");
         return OGS_ERROR;
     }
-    if (!(flags & O_NONBLOCK)) {
+    if (!(flags & O_NONBLOCK))
+    {
         rc = fcntl(fd, F_SETFL, (flags | O_NONBLOCK));
-        if (rc != OGS_OK) {
+        if (rc != OGS_OK)
+        {
             ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno, "F_SETFL failed");
             return OGS_ERROR;
         }
@@ -94,13 +98,16 @@ int ogs_closeonexec(ogs_socket_t fd)
 
     ogs_assert(fd != INVALID_SOCKET);
     flags = fcntl(fd, F_GETFD, NULL);
-    if (flags < 0) {
+    if (flags < 0)
+    {
         ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno, "F_GETFD failed");
         return OGS_ERROR;
     }
-    if (!(flags & FD_CLOEXEC)) {
+    if (!(flags & FD_CLOEXEC))
+    {
         rc = fcntl(fd, F_SETFD, (flags | FD_CLOEXEC));
-        if (rc != OGS_OK) {
+        if (rc != OGS_OK)
+        {
             ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno, "F_SETFD failed");
             return OGS_ERROR;
         }
@@ -119,9 +126,10 @@ int ogs_listen_reusable(ogs_socket_t fd, int on)
 
     ogs_debug("Turn on SO_REUSEADDR");
     rc = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (void *)&on, sizeof(int));
-    if (rc != OGS_OK) {
+    if (rc != OGS_OK)
+    {
         ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno,
-                "setsockopt(SOL_SOCKET, SO_REUSEADDR) failed");
+                        "setsockopt(SOL_SOCKET, SO_REUSEADDR) failed");
         return OGS_ERROR;
     }
 #endif
@@ -138,9 +146,10 @@ int ogs_tcp_nodelay(ogs_socket_t fd, int on)
 
     ogs_debug("Turn on TCP_NODELAY");
     rc = setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, (void *)&on, sizeof(int));
-    if (rc != OGS_OK) {
+    if (rc != OGS_OK)
+    {
         ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno,
-                "setsockopt(IPPROTO_TCP, TCP_NODELAY) failed");
+                        "setsockopt(IPPROTO_TCP, TCP_NODELAY) failed");
         return OGS_ERROR;
     }
 #endif
@@ -162,10 +171,11 @@ int ogs_so_linger(ogs_socket_t fd, int l_linger)
 
     ogs_debug("SO_LINGER:[%d]", l_linger);
     rc = setsockopt(fd, SOL_SOCKET, SO_LINGER,
-            (void *)&l, sizeof(struct linger));
-    if (rc != OGS_OK) {
+                    (void *)&l, sizeof(struct linger));
+    if (rc != OGS_OK)
+    {
         ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno,
-                "setsockopt(SOL_SOCKET, SO_LINGER) failed");
+                        "setsockopt(SOL_SOCKET, SO_LINGER) failed");
         return OGS_ERROR;
     }
 #endif
@@ -182,11 +192,12 @@ int ogs_bind_to_device(ogs_socket_t fd, const char *device)
     ogs_assert(device);
 
     ogs_debug("SO_BINDTODEVICE:[%s]", device);
-    rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, device, strlen(device)+1);
-    if (rc != OGS_OK) {
+    rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, device, strlen(device) + 1);
+    if (rc != OGS_OK)
+    {
         int err = ogs_errno;
         ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno,
-                "setsockopt(SOL_SOCKET, SO_BINDTODEVICE, %s) failed", device);
+                        "setsockopt(SOL_SOCKET, SO_BINDTODEVICE, %s) failed", device);
         if (err == OGS_EPERM)
             ogs_error("You need to grant CAP_NET_RAW privileges to use SO_BINDTODEVICE.");
         return OGS_ERROR;
--- src/mme/s1ap-sctp.c
+++ src/mme/s1ap-sctp.c
@@ -43,14 +43,17 @@ ogs_sock_t *s1ap_server(ogs_socknode_t *node)
 
 #if HAVE_USRSCTP
     sock = ogs_sctp_server(SOCK_SEQPACKET, node->addr, node->option);
-    if (!sock) return NULL;
+    if (!sock)
+        return NULL;
     usrsctp_set_non_blocking((struct socket *)sock, 1);
     usrsctp_set_upcall((struct socket *)sock, usrsctp_recv_handler, NULL);
 #else
+    node->option->sctp.spp_hbinterval = 120000; // nick -- adjust heartbeat interval to 2 minutes
     sock = ogs_sctp_server(SOCK_STREAM, node->addr, node->option);
-    if (!sock) return NULL;
+    if (!sock)
+        return NULL;
     poll = ogs_pollset_add(ogs_app()->pollset,
-            OGS_POLLIN, sock->fd, lksctp_accept_handler, sock);
+                           OGS_POLLIN, sock->fd, lksctp_accept_handler, sock);
     ogs_assert(poll);
 
     node->poll = poll;
@@ -60,7 +63,7 @@ ogs_sock_t *s1ap_server(ogs_socknode_t *node)
     node->cleanup = ogs_sctp_destroy;
 
     ogs_info("s1ap_server() [%s]:%d",
-            OGS_ADDR(node->addr, buf), OGS_PORT(node->addr));
+             OGS_ADDR(node->addr, buf), OGS_PORT(node->addr));
 
     return sock;
 }
@@ -82,7 +85,8 @@ static void usrsctp_recv_handler(struct socket *socket, void *data, int flags)
     int events;
 
     while ((events = usrsctp_get_events(socket)) &&
-           (events & SCTP_EVENT_READ)) {
+           (events & SCTP_EVENT_READ))
+    {
         s1ap_recv_handler((ogs_sock_t *)socket);
     }
 }
@@ -104,18 +108,21 @@ void s1ap_accept_handler(ogs_sock_t *sock)
     ogs_assert(sock);
 
     new = ogs_sock_accept(sock);
-    if (new) {
+    if (new)
+    {
         ogs_sockaddr_t *addr = NULL;
 
         addr = ogs_calloc(1, sizeof(ogs_sockaddr_t));
         ogs_assert(addr);
         memcpy(addr, &new->remote_addr, sizeof(ogs_sockaddr_t));
 
-        ogs_info("eNB-S1 accepted[%s]:%d in s1_path module", 
-            OGS_ADDR(addr, buf), OGS_PORT(addr));
+        ogs_info("eNB-S1 accepted[%s]:%d in s1_path module",
+                 OGS_ADDR(addr, buf), OGS_PORT(addr));
 
         s1ap_event_push(MME_EVENT_S1AP_LO_ACCEPT, new, addr, NULL, 0, 0);
-    } else {
+    }
+    else
+    {
         ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno, "accept() failed");
     }
 }
@@ -135,29 +142,33 @@ void s1ap_recv_handler(ogs_sock_t *sock)
     ogs_assert(pkbuf);
     ogs_pkbuf_put(pkbuf, OGS_MAX_SDU_LEN);
     size = ogs_sctp_recvmsg(
-            sock, pkbuf->data, pkbuf->len, &from, &sinfo, &flags);
-    if (size < 0 || size >= OGS_MAX_SDU_LEN) {
+        sock, pkbuf->data, pkbuf->len, &from, &sinfo, &flags);
+    if (size < 0 || size >= OGS_MAX_SDU_LEN)
+    {
         ogs_error("ogs_sctp_recvmsg(%d) failed(%d:%s)",
-                size, errno, strerror(errno));
+                  size, errno, strerror(errno));
         ogs_pkbuf_free(pkbuf);
         return;
     }
 
-    if (flags & MSG_NOTIFICATION) {
+    if (flags & MSG_NOTIFICATION)
+    {
         union sctp_notification *not =
             (union sctp_notification *)pkbuf->data;
 
-        switch(not->sn_header.sn_type) {
-        case SCTP_ASSOC_CHANGE :
+        switch (not->sn_header.sn_type)
+        {
+        case SCTP_ASSOC_CHANGE:
             ogs_debug("SCTP_ASSOC_CHANGE:"
-                    "[T:%d, F:0x%x, S:%d, I/O:%d/%d]", 
-                    not->sn_assoc_change.sac_type,
-                    not->sn_assoc_change.sac_flags,
-                    not->sn_assoc_change.sac_state,
-                    not->sn_assoc_change.sac_inbound_streams,
-                    not->sn_assoc_change.sac_outbound_streams);
+                      "[T:%d, F:0x%x, S:%d, I/O:%d/%d]",
+                      not->sn_assoc_change.sac_type,
+                      not->sn_assoc_change.sac_flags,
+                      not->sn_assoc_change.sac_state,
+                      not->sn_assoc_change.sac_inbound_streams,
+                      not->sn_assoc_change.sac_outbound_streams);
 
-            if (not->sn_assoc_change.sac_state == SCTP_COMM_UP) {
+            if (not->sn_assoc_change.sac_state == SCTP_COMM_UP)
+            {
                 ogs_debug("SCTP_COMM_UP");
 
                 addr = ogs_calloc(1, sizeof(ogs_sockaddr_t));
@@ -165,11 +176,13 @@ void s1ap_recv_handler(ogs_sock_t *sock)
                 memcpy(addr, &from, sizeof(ogs_sockaddr_t));
 
                 s1ap_event_push(MME_EVENT_S1AP_LO_SCTP_COMM_UP,
-                        sock, addr, NULL,
-                        not->sn_assoc_change.sac_inbound_streams,
-                        not->sn_assoc_change.sac_outbound_streams);
-            } else if (not->sn_assoc_change.sac_state == SCTP_SHUTDOWN_COMP ||
-                    not->sn_assoc_change.sac_state == SCTP_COMM_LOST) {
+                                sock, addr, NULL,
+                                not->sn_assoc_change.sac_inbound_streams,
+                                not->sn_assoc_change.sac_outbound_streams);
+            }
+            else if (not->sn_assoc_change.sac_state == SCTP_SHUTDOWN_COMP ||
+                     not->sn_assoc_change.sac_state == SCTP_COMM_LOST)
+            {
 
                 if (not->sn_assoc_change.sac_state == SCTP_SHUTDOWN_COMP)
                     ogs_debug("SCTP_SHUTDOWN_COMP");
@@ -181,56 +194,58 @@ void s1ap_recv_handler(ogs_sock_t *sock)
                 memcpy(addr, &from, sizeof(ogs_sockaddr_t));
 
                 s1ap_event_push(MME_EVENT_S1AP_LO_CONNREFUSED,
-                        sock, addr, NULL, 0, 0);
+                                sock, addr, NULL, 0, 0);
             }
             break;
 
-        case SCTP_SHUTDOWN_EVENT :
+        case SCTP_SHUTDOWN_EVENT:
             ogs_debug("SCTP_SHUTDOWN_EVENT:[T:%d, F:0x%x, L:%d]",
-                    not->sn_shutdown_event.sse_type,
-                    not->sn_shutdown_event.sse_flags,
-                    not->sn_shutdown_event.sse_length);
+                      not->sn_shutdown_event.sse_type,
+                      not->sn_shutdown_event.sse_flags,
+                      not->sn_shutdown_event.sse_length);
 
             addr = ogs_calloc(1, sizeof(ogs_sockaddr_t));
             ogs_assert(addr);
             memcpy(addr, &from, sizeof(ogs_sockaddr_t));
 
             s1ap_event_push(MME_EVENT_S1AP_LO_CONNREFUSED,
-                    sock, addr, NULL, 0, 0);
+                            sock, addr, NULL, 0, 0);
             break;
 
-        case SCTP_SEND_FAILED :
+        case SCTP_SEND_FAILED:
 #if HAVE_USRSCTP
             ogs_error("SCTP_SEND_FAILED:[T:%d, F:0x%x, S:%d]",
-                    not->sn_send_failed_event.ssfe_type,
-                    not->sn_send_failed_event.ssfe_flags,
-                    not->sn_send_failed_event.ssfe_error);
+                      not->sn_send_failed_event.ssfe_type,
+                      not->sn_send_failed_event.ssfe_flags,
+                      not->sn_send_failed_event.ssfe_error);
 #else
             ogs_error("SCTP_SEND_FAILED:[T:%d, F:0x%x, S:%d]",
-                    not->sn_send_failed.ssf_type,
-                    not->sn_send_failed.ssf_flags,
-                    not->sn_send_failed.ssf_error);
+                      not->sn_send_failed.ssf_type,
+                      not->sn_send_failed.ssf_flags,
+                      not->sn_send_failed.ssf_error);
 #endif
             break;
 
         case SCTP_PEER_ADDR_CHANGE:
-            ogs_warn("SCTP_PEER_ADDR_CHANGE:[T:%d, F:0x%x, S:%d]", 
-                    not->sn_paddr_change.spc_type,
-                    not->sn_paddr_change.spc_flags,
-                    not->sn_paddr_change.spc_error);
+            ogs_warn("SCTP_PEER_ADDR_CHANGE:[T:%d, F:0x%x, S:%d]",
+                     not->sn_paddr_change.spc_type,
+                     not->sn_paddr_change.spc_flags,
+                     not->sn_paddr_change.spc_error);
             break;
         case SCTP_REMOTE_ERROR:
-            ogs_warn("SCTP_REMOTE_ERROR:[T:%d, F:0x%x, S:%d]", 
-                    not->sn_remote_error.sre_type,
-                    not->sn_remote_error.sre_flags,
-                    not->sn_remote_error.sre_error);
+            ogs_warn("SCTP_REMOTE_ERROR:[T:%d, F:0x%x, S:%d]",
+                     not->sn_remote_error.sre_type,
+                     not->sn_remote_error.sre_flags,
+                     not->sn_remote_error.sre_error);
             break;
-        default :
+        default:
             ogs_error("Discarding event with unknown flags:0x%x type:0x%x",
-                    flags, not->sn_header.sn_type);
+                      flags, not->sn_header.sn_type);
             break;
         }
-    } else if (flags & MSG_EOR) {
+    }
+    else if (flags & MSG_EOR)
+    {
         ogs_pkbuf_trim(pkbuf, size);
 
         addr = ogs_calloc(1, sizeof(ogs_sockaddr_t));
@@ -239,14 +254,19 @@ void s1ap_recv_handler(ogs_sock_t *sock)
 
         s1ap_event_push(MME_EVENT_S1AP_MESSAGE, sock, addr, pkbuf, 0, 0);
         return;
-    } else {
-        if (ogs_socket_errno != OGS_EAGAIN) {
+    }
+    else
+    {
+        if (ogs_socket_errno != OGS_EAGAIN)
+        {
             ogs_fatal("ogs_sctp_recvmsg(%d) failed(%d:%s-0x%x)",
-                    size, errno, strerror(errno), flags);
+                      size, errno, strerror(errno), flags);
             ogs_assert_if_reached();
-        } else {
+        }
+        else
+        {
             ogs_error("ogs_sctp_recvmsg(%d) failed(%d:%s-0x%x)",
-                    size, errno, strerror(errno), flags);
+                      size, errno, strerror(errno), flags);
         }
     }
 
